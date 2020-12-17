f1c4d43af6f51a002be961a058f77e6dfe3d6592
rifkiramdhann <ramadhanrifki85@gmail.com>
2020-12-17 11:30:21 +0700

    update detail

--- app/src/main/java/com/example/stayarta/DetailActivity.java
+++ app/src/main/java/com/example/stayarta/DetailActivity.java
@@ -3,6 +3,10 @@ package com.example.stayarta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        TextView tv_title2 = findViewById(R.id.tv_title2);
+        TextView tv_nomor = findViewById(R.id.tv_nomor);
+        TextView tv_alamat = findViewById(R.id.tv_alamat);
+        ImageView iv_hotel2 = findViewById(R.id.iv_hotel2);
+
+        tv_title2.setText(getIntent().getStringExtra("nama"));
+        tv_nomor.setText("Nomor Telepon: "+getIntent().getStringExtra("nomor"));
+        tv_alamat.setText("Alamat: "+getIntent().getStringExtra("alamat"));
+        Glide.with(getApplicationContext())
+                .load(getIntent()
+                        .getStringExtra("gambar")).error(R.mipmap.logo).into(iv_hotel2);
     }
 }
